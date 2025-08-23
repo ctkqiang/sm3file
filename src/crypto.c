b47a1fdc899fcc246eafbb9f7bffc75c60b426cd
🇨🇳钟智强 『江西青垣科技』 <johnmelodymel@qq.com>
2025-08-24 00:41:14 +0800

    feat(加密): 添加SM4和随机数生成支持并完善PBKDF2实现
    添加对SM4加密和随机数生成的支持，同时完善PBKDF2密钥派生函数的实现
    更新.vscode设置以包含新增的头文件

--- src/crypto.c
+++ src/crypto.c
@@ -3,7 +3,24 @@
 
 #include "../include/crypto.h"
 #include "../external/GmSSL/include/gmssl/sm3.h"
+#include "../external/GmSSL/include/gmssl/sm4.h"
+#include "../external/GmSSL/include/gmssl/rand.h"
 
 int pbkdf2_sm3(const char *password, size_t password_len, const uint8_t *salt, size_t salt_len, uint8_t *key) {
-    
+    if (gmssl_pbkdf2_hmac_sm3(password, password_len, salt, salt_len, PBKDF2_ITERATION, SM4_KEY_LENGTH, key) != 0x1) {
+        fprintf(stderr, "PBKDF2密钥派生失败");
+
+        return -0x1;
+    }
+
+    return 0x0;
+}
+
+int generate_random_bytes(uint8_t *data, size_t len) {
+    if (gmssl_rand_bytes(data, len) != 0x1) {
+        fprintf(stderr, "生成随机数失败");
+        return -0x1;
+    }
+
+    return 0x0;
 }
