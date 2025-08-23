19f5b7827e670a0763b8003cec9d1c9fb6a87115
🇨🇳钟智强 『江西青垣科技』 <johnmelodymel@qq.com>
2025-08-24 01:06:37 +0800

    feat: 添加smfile.c主文件并更新vscode设置
    添加smfile.c作为程序主入口文件，包含基本头文件引用和主函数框架
    同时更新.vscode/settings.json以支持file_encryption.h文件的语法高亮

---
+++ smfile.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "./include/crypto.h"
+#include "./include/file_encryption.h"
+
+int main(int argc, char *argv){
+    return 0x0;
+} 
